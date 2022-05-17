6d361070052e76dfaa0422226d01c69ddd9e1416
Icodak <24356594+Icodak@users.noreply.github.com>
2022-05-17 11:34:37 +0200

    app

--- notGimp/Application.cpp
+++ notGimp/Application.cpp
@@ -2,11 +2,34 @@
 #include "opencv2/opencv.hpp"
 
 ngp::Application::Application() {
-	cv::Mat frame = cv::Mat::zeros(100, 100, 0);
+	s_Mat = cv::Mat::zeros(100, 100, 0);
 	//Init event handlers
-	imshow("notGimp", frame);
+	cv::imshow("notGimp", s_Mat);
 }
 
 void ngp::Application::Run() {
-	cv::waitKey(0);
+	while (ReadFile());
+	
+	//stop
+	while (1) {
+		cv::imshow("notGimp", s_Mat);
+		char c = (char)cv::waitKey(25);
+		if (c == 27) break;
+	}
+}
+
+
+int ngp::Application::ReadFile() {
+	std::string input;
+	std::getline(std::cin, input);
+	try {
+		std::string image_path = cv::samples::findFile(input);
+		s_Mat = cv::imread(image_path, cv::IMREAD_COLOR);
+		std::cout << "Image opened at" << image_path << std::endl;
+	} catch (cv::Exception){
+		std::cout << "Could not read the image: " << input << " please provide valid link" << std::endl;
+		return 1;
+	}
+
+	return 0;
 }
